19c111309c665b1a744dbefb8ab41b74f3d8bd23
NekoDev <NekoDevStrike@users.noreply.github.com>
2023-11-10 08:57:27 -0600

    adicion de arquitectura 64

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVE1TzZSUC8vLy8vQUFBQm1ZN1d2TEdmM0VKdGlLSThIUVJRZWRWOWZuSXg1U0ZPTWFQb2pRemVJUmQ2NitJQ2hXRkEvWThkQS9VMnU3NWtaR2w5ZHpLQ2NmcFNLZHkxd0o4aFQ0RnlGd1hvYXRwa0JsZzJLdmJpUjRESEwyNU1KREREZTI5VHVBL2E2M0hGWUtuTVo0K2UzbGdnUUo1Y0h4enNYRlc2WlFsbWU2RzdQc1hDK0EwUUdQNkprZ3ZNd3BJNUpVOVZxYzBISWZ0ZCtrd3N1UlJHL2N2THlwbkhSYXlXdi9HVEdia2dhV3BZMjJ2RDgxNUlJMktEaVdBU1FxeVhVNkVROUtjekdkYzN4ZUxVQTdZSS9ESTdhdGw1UzF1TUhtYXI4RVc4Wkora1JHS0ErdFFsYmkyVWZSUU5nZDhTZFV3NEliWVVKSmw5MUVzNUlsdEZWMlAzc20rN3ZndlBGdndSVVdhVTJOQitHRDhuamI3RnJrU3c=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
